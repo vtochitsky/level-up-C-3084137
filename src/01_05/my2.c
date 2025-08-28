d35078a99a24023394fe3e2a4cfd32b7b42423e1
@vtochitsky <vtochitsky@gmail.com>
2025-08-28 19:18:29 +0300

    01_05 my2 using memmove

---
+++ src/01_05/my2.c
@@ -0,0 +1,99 @@
+#include <stdio.h>
+#include <string.h> /* for memmove */
+#include <assert.h> /* for assert */
+
+//const char original_deck[] = {'A', 'B', 'C', 'D', 'E', 'F'};
+//const char original_deck[] = {'A', 'B', 'C', 'D', 'E'};
+
+const char original_deck[] = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K',
+                              'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V',
+                              'W', 'X', 'Y', 'Z',
+			      '0', '1', '2', '3', '4','5','6','7','8','9'};
+#define LENGTH (sizeof(original_deck) / sizeof(original_deck[0u]))
+
+//const char original_deck[] = "ABCDEFGHIJKLMNOPQRSTVWUXYZ";
+//#define LENGTH (sizeof(original_deck) / sizeof(original_deck[0u]) - 1u) /* del nul terminator. it can be calculated by strlen() without -1 */
+
+void print_deck(const char *deck, const size_t length);
+int compare_decks(const char *deck1, const char *deck2, const size_t length);
+void perfect_shuffle(char *deck, const size_t length);
+
+int main(void)
+{
+  char deck[LENGTH];
+  size_t shuffles_counter = 0u;
+
+  printf("Original deck: ");
+  print_deck(original_deck, LENGTH);
+  printf("\n");
+
+  /* copy original deck to 'working copy' deck */
+  memmove(deck, original_deck, LENGTH);
+
+  do
+  {
+    perfect_shuffle(deck, LENGTH);
+    shuffles_counter++;
+    printf("%-5zu: ", shuffles_counter);
+    print_deck(deck, LENGTH);
+    printf("\n");
+  } while (1 != compare_decks(original_deck, deck, LENGTH));
+
+  printf("Deck restored after %zu iterations\n", shuffles_counter);
+
+  return 0;
+}
+
+void print_deck(const char *deck, const size_t length)
+{
+  size_t i;
+  for (i = 0u; i < length; i++)
+  {
+    printf("%c", deck[i]);
+  }
+}
+
+/*
+The function compares two decks. If they are the same then it returns 1 oterwise 0.
+*/
+int compare_decks(const char *deck1, const char *deck2, const size_t length)
+{
+  size_t i;
+  for (i = 0u; i < length; i++)
+  {
+    if (deck1[i] != deck2[i])
+    {
+      return 0;
+    }
+    else
+    {
+      /* nothing */
+    }
+  }
+
+  return 1;
+}
+
+void perfect_shuffle(char *deck, const size_t length)
+{
+  assert(length > 0);
+  char buffer[length]; /* VLA */
+
+  const size_t subtrahend = (0u == length%2u ? 1u : 0u);
+
+  size_t j = 0u;
+  size_t i;
+  for (i = 0u; i < length - subtrahend; i += 2u)
+  {
+    buffer[i] = deck[j];
+    j++;
+  }
+
+  for (i = 1u; i < length; i += 2u)
+  {
+    buffer[i] = deck[j];
+    j++;
+  }
+
+  memmove(deck, buffer, length);
+}
