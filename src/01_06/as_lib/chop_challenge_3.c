33c5d92a3beaf9f949efe162971cf14cdf82b109
vt <vt>
2025-09-01 23:12:09 +0300

    functions were implemented in one unit ( but still with one global buffer - wrong for multi-thread applications)

---
+++ src/01_06/as_lib/chop_challenge_3.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include "functions.h"
+
+int main()
+{
+	char string[] = "Once upon a time, there was a string";
+
+	printf("Original string: %s\n",string);
+	printf("Left %d characters: %s\n",16,left(string,16));
+	printf("Right %d characters: %s\n",18,right(string,18));
+	printf("Middle %d characters: %s\n",11,mid(string,12,11)); /* 12 is correct */
+
+	return(0);
+}
---
+++ src/01_06/as_lib/functions.h
@@ -0,0 +1,10 @@
+#ifndef _FUNCTIONS_H_
+#define _FUNCTIONS_H_
+
+#define BUFFER_LENGTH 256u
+
+char *left(char *s, int len);
+char *right(char *s, int len);
+char *mid(char *s, int offset, int len);
+
+#endif /* _FUNCTIONS_H_ */
