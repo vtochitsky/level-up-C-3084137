18613311a68d47c05fbe77b55666fe646241989f
vt <vt>
2025-09-02 15:09:28 +0300

    01_06 four  multi threads

--- src/01_06/multi_thread_1/chop_challenge_4.c
+++ src/01_06/multi_thread_1/chop_challenge_4.c
@@ -13,9 +13,9 @@ enum
   N_RIGTH = 100,
   N_MID = 100,
   MAX_TEST_LEN = 100,
-  T_MID = 10,
-  T_RIGTH = 8,
-  T_LEFT = 12,
+  T_MID = 6,
+  T_RIGTH = 3,
+  T_LEFT = 4,
 };
 
 // Assume these are implemented elsewhere
@@ -94,22 +94,46 @@ void *call_mid(void *args)
   return NULL;
 }
 
+void *call_left2(void *args)
+{
+  ThreadArgs *targs = (ThreadArgs *)args;
+  for (size_t i = 0u; i < N_LEFT; i++)
+  {
+    char buf[MAX_TEST_LEN];
+    strcpy(buf, left(targs->input, targs->len));
+    printf("Left(%d): %s\n", targs->len, buf);
+    if (strcmp(buf, "Once"))
+    {
+      puts("LEFT ERROR");
+      glb_error_cnt++;
+    }
+    struct timespec ts;
+    ts.tv_sec = 0;
+    ts.tv_nsec = T_LEFT * 1000000; // T_LEFT milliseconds
+    nanosleep(&ts, NULL);
+  }
+  return NULL;
+}
+
 int main()
 {
   char *input = strdup("Once upon a time, there was a string");
 
-  pthread_t t1, t2, t3;
+  pthread_t t1, t2, t3, t4;
   ThreadArgs args1 = {input, 0, 16};  // left 16 chars
   ThreadArgs args2 = {input, 0, 18};  // right 18 chars
   ThreadArgs args3 = {input, 12, 11}; // mid from offset 12, length 11
+  ThreadArgs args4 = {input, 0, 4};   // left 14 chars
 
   pthread_create(&t1, NULL, call_left, &args1);
   pthread_create(&t2, NULL, call_right, &args2);
   pthread_create(&t3, NULL, call_mid, &args3);
+  pthread_create(&t4, NULL, call_left2, &args4);
 
   pthread_join(t1, NULL);
   pthread_join(t2, NULL);
   pthread_join(t3, NULL);
+  pthread_join(t4, NULL);
 
   free(input);
 
