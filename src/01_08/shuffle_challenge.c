175b22d968379a838d394b7b44e47d47d55bf2b4
vt <vt>
2025-09-04 12:11:00 +0300

    01_08 implemented using somwthing like queue with unique items - it seems too complex

--- src/01_08/shuffle_challenge.c
+++ src/01_08/shuffle_challenge.c
@@ -3,6 +3,7 @@
 #include <time.h>
 
 #define PSIZE 20
+#define REPEAT 15
 
 int main()
 {
@@ -18,18 +19,65 @@ int main()
 	int frequency[PSIZE];
 	int x, r, count;
 
+	int recent[REPEAT];
+	int tail;
+	int r_ok;
+
 	/* initialize */
 	srand((unsigned)time(NULL));
 	for (x = 0; x < PSIZE; x++)
 		frequency[x] = 0;
+
+	tail = 0;
+	for (x = 0; x < REPEAT; x++)
+		recent[x] = 0;
 
 	/* run through the playlist randomly */
 	puts("Playlist:");
 	count = 0;
 	while (count < 100)
 	{
-		/* does r appear in recent[]? */
-		r = rand() % PSIZE; /* random value */
+
+		/* generate random value which appears not often then REPEAT */
+		r_ok = 0;
+		do
+		{
+			r = rand() % PSIZE; /* random value */
+
+			/* does r appear in recent[]? */
+			for (int i = 0; i < REPEAT; i++)
+			{
+				if (r == recent[i])
+				{
+					/* generate next r */
+					r_ok = 0;
+					break;
+				}
+				else
+				{
+					/* exit */
+					r_ok = 1;
+				}
+			}
+		} while (r_ok == 0);
+
+		/* store new 'r' to queue (first dequeue if needed then enqueue )*/
+
+		/* dequeue */
+		if (tail >= REPEAT)
+		{
+			/* throw out recent[0] and shift every item to the left */
+			for (int i = 0; i < (REPEAT - 1); i++)
+			{
+				recent[i] = recent[i + 1];
+			}
+			tail = REPEAT - 1;
+		}
+
+		/* enqueue */
+		recent[tail] = r;
+		tail++;
+
 		printf("%3d: Now Playing '%s'\n",
 					 count + 1,
 					 playlist[r]);
