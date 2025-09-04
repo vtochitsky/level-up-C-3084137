20569355e7528bc2b9e10546313cb92104ee2e0a
vt <vt>
2025-09-04 11:24:31 +0300

    01_08 just reformating

--- src/01_08/shuffle_challenge.c
+++ src/01_08/shuffle_challenge.c
@@ -7,42 +7,39 @@
 int main()
 {
 	char *playlist[PSIZE] = {
-		"Like a Rolling Stone", "Satisfaction", "Imagine",
-		"What's Going On", "Respect", "Good Vibrations",
-		"Johnny B. Goode", "Hey Jude", "What'd I Say",
-		"Smells Like Teen Spirit", "My Generation",
-		"Yesterday", "Blowin' in the Wind", "Purple Haze",
-		"London Calling", "I Want to Hold Your Hand",
-		"Maybellene", "Hound Dog", "Let It Be",
-		"A Change Is Gonna Come"
-	};
+			"Like a Rolling Stone", "Satisfaction", "Imagine",
+			"What's Going On", "Respect", "Good Vibrations",
+			"Johnny B. Goode", "Hey Jude", "What'd I Say",
+			"Smells Like Teen Spirit", "My Generation",
+			"Yesterday", "Blowin' in the Wind", "Purple Haze",
+			"London Calling", "I Want to Hold Your Hand",
+			"Maybellene", "Hound Dog", "Let It Be",
+			"A Change Is Gonna Come"};
 	int frequency[PSIZE];
-	int x,r,count;
+	int x, r, count;
 
 	/* initialize */
-	srand( (unsigned)time(NULL) );
-	for(x=0;x<PSIZE;x++)
+	srand((unsigned)time(NULL));
+	for (x = 0; x < PSIZE; x++)
 		frequency[x] = 0;
-
 
 	/* run through the playlist randomly */
 	puts("Playlist:");
 	count = 0;
-	while(count<100)
+	while (count < 100)
 	{
 		/* does r appear in recent[]? */
-		r = rand() % PSIZE;		/* random value */
+		r = rand() % PSIZE; /* random value */
 		printf("%3d: Now Playing '%s'\n",
-				count+1,
-				playlist[r]
-			  );
+					 count + 1,
+					 playlist[r]);
 		frequency[r]++;
 		count++;
 	}
 
 	puts("Song frequency:");
-	for(x=0;x<PSIZE;x++)
-		printf("%s: %d\n",playlist[x],frequency[x]);
+	for (x = 0; x < PSIZE; x++)
+		printf("%s: %d\n", playlist[x], frequency[x]);
 
-	return(0);
+	return (0);
 }
