a046c2418d2e8525dc85d37955a33b16bb073871
vt <vt>
2025-09-06 15:18:49 +0300

    01_09 implemented without board printing out

---
+++ src/01_09/my_solution.c
@@ -0,0 +1,105 @@
+#include <stdio.h>
+#include <stdlib.h> /* for srand() and rand() */
+#include <time.h>   /* for time() */
+
+enum
+{
+  HSIZE = 8, /* horizontal board size */
+  VSIZE = 8, /* vertical board size */
+};
+
+enum
+{
+  NO_MOVE,
+  UP_LEFT,
+  UP,
+  UP_RIGHT,
+  RIGHT,
+  DOWN_RIGHT,
+  DOWN,
+  DOWN_LEFT,
+  LEFT,
+  /* The last item is total number*/
+  NUMBER_OF_MOVEMENTS
+};
+
+int is_king_on_board(int x_pos, int y_pos, int h_size, int v_size);
+
+void move_king(int *x_pos, int *y_pos);
+
+int main(void)
+{
+  int king_x = 5; /* start position x */
+  int king_y = 4; /* start position y */
+  int king_movement_counter = 0;
+
+  srand((unsigned)time(NULL));
+
+  while (is_king_on_board(king_x, king_y, HSIZE, VSIZE))
+  {
+    move_king(&king_x, &king_y);
+    king_movement_counter++;
+  }
+
+  printf("Total number of king's movements: %d\n", king_movement_counter);
+
+  return 0;
+}
+
+int is_king_on_board(int x_pos, int y_pos, int h_size, int v_size)
+{
+  int result = 0;
+
+  if (x_pos < 0 || h_size <= x_pos || y_pos < 0 || v_size <= y_pos)
+  {
+    result = 0;
+  }
+  else
+  {
+    result = 1;
+  }
+
+  return result;
+}
+
+void move_king(int *x_pos, int *y_pos)
+{
+  int r = rand() % NUMBER_OF_MOVEMENTS;
+  switch (r)
+  {
+  case (NO_MOVE):
+    /* nothing to do */
+    break;
+  case (UP_LEFT):
+    (*x_pos)--;
+    (*y_pos)--;
+    break;
+  case (UP):
+    (*y_pos)--;
+    break;
+  case (UP_RIGHT):
+    (*x_pos)++;
+    (*y_pos)--;
+    break;
+  case (RIGHT):
+    (*x_pos)++;
+    break;
+  case (DOWN_RIGHT):
+    (*x_pos)++;
+    (*y_pos)++;
+    break;
+  case (DOWN):
+    (*y_pos)++;
+    break;
+  case (DOWN_LEFT):
+    (*x_pos)--;
+    (*y_pos)++;
+    break;
+  case (LEFT):
+    (*x_pos)--;
+    break;
+  default:
+    /* nothing to do */
+    break;
+  }
+}
