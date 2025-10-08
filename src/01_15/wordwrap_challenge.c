1ca0cfc905ac2f9676cbe9408cc75b76e10e0ac0
vt <vt>
2025-10-09 00:31:05 +0300

    01_15 initial draft implementation

--- src/01_15/wordwrap_challenge.c
+++ src/01_15/wordwrap_challenge.c
@@ -1,8 +1,11 @@
 #include <stdio.h>
+#include <ctype.h>
 
 #define W_MAX 100
 #define W_MIN 16
 #define W_DEFAULT 40
+
+void wordwrap(char *text, const unsigned int width);
 
 int main(int argc, char *argv[])
 {
@@ -22,9 +25,56 @@ When in eternal lines to time thou grow'st:\n\
 So long as men can breathe or eyes can see, \
 So long lives this, and this gives life to thee.";
 
-	puts("Default text:");
-	puts("");
-	printf("%s\n", text);
-	
-	return(0);
+	// puts("Default text:");
+	// puts("");
+	// printf("%s\n", text);
+
+	// char *runner = text;
+
+	// while (*runner)
+	// {
+	// 	if (*runner != '\n')
+	// 		putchar(*runner);
+	// 	runner++;
+	// }
+
+	// runner = text;
+	// while (*runner)
+	// {
+	// 	putchar(*runner);
+	// 	runner++;
+	// }
+
+	unsigned int width = 40u;
+
+	wordwrap(text, width);
+
+	return (0);
+}
+
+void wordwrap(char *text, const unsigned int width)
+{
+	unsigned int line_length = 0u;
+	char *line_start = text;
+
+	do
+	{
+		line_length = 0u;
+		/* go forward till the end of the text or up to 'width' */
+		while (0 != *(line_start + line_length) && line_length < width)
+		{
+			line_length++;
+		}
+
+		/* go back till character is alpha or digit */
+		while (isalnum(*(line_start + line_length - 1)))
+		{
+			line_length--;
+		}
+
+		printf("%.*s\n", line_length, line_start);
+
+		line_start += line_length;
+
+	} while (0 != *(line_start + line_length));
 }
